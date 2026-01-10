334dbb6af8f7c0efd23ee658fcc6d5f82232f503
annaeden1 <annaeden1@gmail.com>
2026-01-10 19:01:12 +0200

    add edit and delete student functionality

--- app/src/main/java/com/idz/studentapp/EditStudentActivity.kt
+++ app/src/main/java/com/idz/studentapp/EditStudentActivity.kt
@@ -1,12 +1,28 @@
 package com.idz.studentapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.idz.studentapp.models.Model
+import com.idz.studentapp.models.Student
 
 class EditStudentActivity : AppCompatActivity() {
+    private var studentList = Model.shared.students
+    private var studentPos: Int? = null
+    private var student: Student? = null
+    private var studentNameEdit: EditText? = null
+    private var studentIdEdit: EditText? = null
+    private var studentPhoneNumberEdit: EditText? = null
+    private var studentAddressEdit: EditText? = null
+    private var studentChecked: CheckBox? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +32,58 @@ class EditStudentActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        studentNameEdit = findViewById(R.id.editTextEditName)
+        studentIdEdit = findViewById(R.id.editTextEditID)
+        studentPhoneNumberEdit = findViewById(R.id.editTextEditPhone)
+        studentAddressEdit = findViewById(R.id.editTextEditAddress)
+        studentChecked = findViewById(R.id.checkBoxEditChecked)
+
+        student = intent.getParcelableExtra<Student>("student")
+        studentPos = intent.getIntExtra("position", -1)
+
+        student?.let{displayStudentDetails(it)}
+
+        findViewById<Button>(R.id.buttonEditSave).setOnClickListener{saveEditedStudent()}
+        findViewById<Button>(R.id.buttonDelete).setOnClickListener{deleteEditedStudent()}
+        findViewById<Button>(R.id.buttonEditCancel).setOnClickListener{finish()}
+    }
+
+    private fun displayStudentDetails(student: Student) {
+        studentNameEdit?.setText(student.name)
+        studentIdEdit?.setText(student.id)
+        studentPhoneNumberEdit?.setText(student.phone)
+        studentAddressEdit?.setText(student.address)
+        studentChecked?.isChecked = student.isChecked
+    }
+
+    private fun saveEditedStudent() {
+        // Get the updated data from the EditTexts
+        val updatedName = studentNameEdit?.text.toString()
+        val updatedId = studentIdEdit?.text.toString()
+        val updatedPhone = studentPhoneNumberEdit?.text.toString()
+        val updatedAddress = studentAddressEdit?.text.toString()
+        val updatedCheck = studentChecked?.isChecked
+
+        // Update the student object with new data
+        student?.name = updatedName
+        student?.id = updatedId
+        student?.phone = updatedPhone
+        student?.address = updatedAddress
+        student?.isChecked = updatedCheck ?: false
+        studentList[studentPos!!] = student!!
+
+        val resultIntent = Intent()
+        resultIntent.putExtra("updatedStudent", student)  // Pass the updated student object
+        setResult(RESULT_OK, resultIntent)
+
+        Toast.makeText(this, "Student updated", Toast.LENGTH_SHORT).show()
+        finish()
+    }
+
+    private fun deleteEditedStudent() {
+        studentPos?.let {studentList.removeAt(it)}
+        setResult(RESULT_OK, Intent())
+        finish()
     }
 }
